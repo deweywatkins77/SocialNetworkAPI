abb758dd56fae6deafccc4ccfcb68c737b2922de
Dewey Watkins <deweywatkins77@gmail.com>
2023-05-31 13:56:59 -0400

    added seedData file, updated connection.js to allow localhost connect, and created virutal for user model

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const { connect, connection } = require('mongoose');
+const connectionString = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/'
 
-connect(process.env.MONGO_URI, {
+connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
---
+++ utils/seedData.js
@@ -0,0 +1,24 @@
+// user collection data
+const usernames = [
+    'mongDB',
+    'user1',
+    'userB',
+    'Plato',
+    'Tesla',
+    'FDR',
+    'Amber',
+    'LastUser53'
+]
+  
+const userEmail = [
+    'mongDB@gmail.com',
+    'user1@gmail.com',
+    'userB@gmail.com',
+    'Plato@gmail.com',
+    'Tesla@gmail.com',
+    'FDR@gmail.com',
+    'Amber@gmail.com',
+    'LastUser53@gmail.com'
+]
+
+module.exports = { usernames, userEmail };
