eac1bb3406fd9ae03a7f05f21219f9d944eacee9
Dewey Watkins <deweywatkins77@gmail.com>
2023-05-31 13:32:58 -0400

    removed sudo code from models

--- models/users.js
+++ models/users.js
@@ -1,27 +1,3 @@
-const { Schema, Types } = require('mongoose');
-
-// * `username`
-//   * String
-//   * Unique
-//   * Required
-//   * Trimmed
-
-// * `email`
-//   * String
-//   * Required
-//   * Unique
-//   * Must match a valid email address (look into Mongoose's matching validation)
-
-// * `thoughts`
-//   * Array of `_id` values referencing the `Thought` model
-
-// * `friends`
-//   * Array of `_id` values referencing the `User` model (self-reference)
-
-// **Schema Settings**:
-
-// Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query.
-
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
