40f00b409a539d7333bc205cadbf23c69dbb514b
Dewey Watkins <deweywatkins77@gmail.com>
2023-06-01 15:39:57 -0400

    updated seedData and seed.js to randomize database with all attributes used

--- utils/seed.js
+++ utils/seed.js
@@ -1,28 +1,61 @@
 const connection = require('../config/connection')
 const { Users, Thoughts } = require('../models')
-const { usernames, userEmail } = require('./seedData')
+const { usernames, userEmail, thoughtData, reactionData } = require('./seedData')
 
 connection.on('error', (err) => err)
 
 connection.once('open', async () => {
-  console.log('connected');
+  console.log('connected')
 
-// Drop existing users
-await Users.deleteMany({});
+// Drop existing users and thoughts
+await Users.deleteMany({})
+await Thoughts.deleteMany({})
 
-// Drop existing thoughts
-await Thoughts.deleteMany({});
-
-const users = []
-for (i=0; i < usernames.length; i++){
-    users.push({username:usernames[i], email:userEmail[i] });
+//create random function for generating random data, our seed data vars are all arrays so we just need this
+function rand(data){
+  return data[Math.floor(Math.random()*(data.length))]
 }
 
-//insert user data into collection
-await Users.collection.insertMany(users);
+//build user records
+let users = []
+for (i=0; i < usernames.length; i++){
+  users.push({username:usernames[i], email:userEmail[i] })
+}
+
+//build thought and reaction records
+let thoughtsArr = []
+let reactionsArr = []
+
+for (i=0; i < thoughtData.length; i++){
+  thoughtsArr.push({
+    thoughtText:thoughtData[i], 
+    username:rand(usernames)
+  })
+
+  reactionsArr.push({
+    reactionBody:reactionData[i], 
+    username:rand(usernames)
+  })
+}
+
+//insert data into collections
+await Users.collection.insertMany(users)
+
+let randNum = Math.floor(Math.random()*4+2)
+for (i=1; i <= randNum; i++){
+  let friend = await Users.findOne({username:rand(usernames)})
+  await Users.updateOne({username:usernames[i-1]},{$push :{friends:friend._id}})
+}
+
+for (const elem of thoughtsArr){
+  let newRecord = await Thoughts.create(elem)
+  await Users.updateOne({username:newRecord.username},{$push :{thoughts:newRecord._id}})
+  await Thoughts.updateOne({_id:newRecord._id}, {$push:{reactions:reactionsArr[thoughtsArr.indexOf(elem)]}})
+}
 
 // Log out the seed data to indicate what should appear in the database
 console.table(users)
+console.table(thoughtsArr)
 console.info('Seeding complete!')
 process.exit(0)
 })
--- utils/seedData.js
+++ utils/seedData.js
@@ -1,24 +1,44 @@
-// user collection data
 const usernames = [
     'mongDB',
     'user1',
+    'Amber',
     'userB',
     'Plato',
     'Tesla',
     'FDR',
-    'Amber',
     'LastUser53'
 ]
   
 const userEmail = [
     'mongDB@gmail.com',
     'user1@gmail.com',
+    'Amber@gmail.com',
     'userB@gmail.com',
     'Plato@gmail.com',
     'Tesla@gmail.com',
     'FDR@gmail.com',
-    'Amber@gmail.com',
     'LastUser53@gmail.com'
 ]
 
-module.exports = { usernames, userEmail };
+const thoughtData =[
+    "thought text test 1",
+    "thought text test 2",
+    "thought text test 3",
+    "thought text test 4",
+    "thought text test 5",
+    "thought text test 6",
+    "thought text test 7",
+    "thought text test 8"
+]
+
+const reactionData =[
+    "reaction 1",
+    "reaction 2",
+    "reaction 3",
+    "reaction 4",
+    "reaction 5",
+    "reaction 6",
+    "reaction 7",
+    "reaction 8"
+]
+module.exports = {usernames, userEmail, thoughtData, reactionData}
