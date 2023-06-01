37d0932c8fa9c4b5c5ec60bf6ba2fa2a731a928e
Dewey Watkins <deweywatkins77@gmail.com>
2023-06-01 00:42:02 -0400

    removed more sudo code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const thoughtRoutes = require('./thoughtRoutes')
 const userRoutes = require('./userRoutes')
-// const reactionRoutes = require('./reactionRoutes')
+const reactionRoutes = require('./reactionRoutes')
 
 router.use('/users', userRoutes)
 router.use('/thoughts', thoughtRoutes)
-// router.use('/reaction', reactionRoutes)
+router.use('/reaction', reactionRoutes)
 
 module.exports = router
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,25 +1,3 @@
-// **`/api/thoughts`**
-
-// * `GET` to get all thoughts
-
-// * `GET` to get a single thought by its `_id`
-
-// * `POST` to create a new thought (don't forget to push the created thought's `_id` to the associated user's `thoughts` array field)
-
-// ```json
-// // example data
-// {
-//   "thoughtText": "Here's a cool thought...",
-//   "username": "lernantino",
-//   "userId": "5edff358a0fcb779aa7b118b"
-// }
-// ```
-
-// * `PUT` to update a thought by its `_id`
-
-// * `DELETE` to remove a thought by its `_id`
-
-// ---
 const router = require('express').Router();
 const {
     getThoughts,
@@ -27,9 +5,6 @@ const {
     createThought,
     updateThought,
     delThought
-//   deleteStudent,
-//   addAssignment,
-//   removeAssignment,
 } = require('../../controller/thoughtController');
 
 // /api/thoughts
@@ -39,21 +14,5 @@ router.route('/:id')
     .get(getThoughtById)
     .put(updateThought)
     .delete(delThought)
-
-// /api/users
-// router.route('/').get(getUsers).post(createUser);
-// router.route('/:id')
-//   .get(getUserById)
-//   .put(updateUser)
-//   .delete(delUser)
-
-// // /api/students/:studentId
-// router.route('/:studentId').get(getSingleStudent).delete(deleteStudent);
-
-// // /api/students/:studentId/assignments
-// router.route('/:studentId/assignments').post(addAssignment);
-
-// // /api/students/:studentId/assignments/:assignmentId
-// router.route('/:studentId/assignments/:assignmentId').delete(removeAssignment);
 
 module.exports = router;
