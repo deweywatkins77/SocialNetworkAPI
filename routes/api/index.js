86a515c6655ce1691b18876378bf77fd5056a9af
Dewey Watkins <deweywatkins77@gmail.com>
2023-06-01 00:49:03 -0400

    removed unused files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router()
 const thoughtRoutes = require('./thoughtRoutes')
 const userRoutes = require('./userRoutes')
-const reactionRoutes = require('./reactionRoutes')
 
 router.use('/users', userRoutes)
 router.use('/thoughts', thoughtRoutes)
-router.use('/reaction', reactionRoutes)
 
 module.exports = router
